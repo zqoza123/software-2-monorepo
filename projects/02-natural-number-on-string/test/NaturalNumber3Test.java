2cdbd6ecc3f6476f877a040288196f93abb21cc7
David Park <111151877+zqoza123@users.noreply.github.com>
2025-01-28 06:31:55 -0500

    Update NaturalNumber3Test.java

--- projects/02-natural-number-on-string/test/NaturalNumber3Test.java
+++ projects/02-natural-number-on-string/test/NaturalNumber3Test.java
@@ -12,29 +12,17 @@ public class NaturalNumber3Test extends NaturalNumberTest {
 
     @Override
     protected final NaturalNumber constructorTest(int i) {
-
-        // TODO - fill in body
-
-        // This line added just to make the component compilable.
-        return null;
+        return new NaturalNumber3(i);
     }
 
     @Override
     protected final NaturalNumber constructorTest(String s) {
-
-        // TODO - fill in body
-
-        // This line added just to make the component compilable.
-        return null;
+        return new NaturalNumber3(s);
     }
 
     @Override
     protected final NaturalNumber constructorTest(NaturalNumber n) {
-
-        // TODO - fill in body
-
-        // This line added just to make the component compilable.
-        return null;
+        return new NaturalNumber3(n);
     }
 
     @Override
